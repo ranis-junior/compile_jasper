1b788b099e71937905953d11e10f3632362c61b3
ranishot <ranishot@hotmail.com>
2025-10-31 00:06:50 -0300

    changing Dockerfile

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker rm $(docker ps -qa)
-docker rmi $(docker images -q)
+#docker rm $(docker ps -qa)
+#docker rmi $(docker images -q)
 docker build -t ranishot/compile_jasper:latest .
-docker image prune --force
 #docker run -d ranishot/nubank_api:latest
